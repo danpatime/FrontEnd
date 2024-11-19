6d27147780d7354c2d9e90adbdddc3ae1105e461
aong13 <oaoa13@pukyong.ac.kr>
2024-11-19 20:52:47 +0900

    :fire: hotfix: router 관련 기본 설정 수정

--- src/Router.js
+++ src/Router.js
@@ -1,23 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AlbaSearchPage from './pages/AlbaSearchPage';
-import MyPage from './pages/MyPage/MyPage';
-import Login from './pages/LoginPage';
-import Signup from './pages/SignupPage';
-import ResumePage from './pages/ResumePage';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AlbaSearchPage from "./pages/AlbaSearchPage";
+import MyPage from "./pages/MyPage/MyPage";
+import Login from "./pages/LoginPage";
+import Signup from "./pages/SignupPage";
+import ResumePage from "./pages/ResumePage";
 
 const AppRouter = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/alba/search" element={<AlbaSearchPage />} />
-        <Route path="/alba/resume/:id" element={<ResumePage />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/alba/search" element={<AlbaSearchPage />} />
+      <Route path="/alba/resume/:id" element={<ResumePage />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 };
 
