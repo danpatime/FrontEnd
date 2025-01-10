e222f8c534af1ff0ef6115d30d6e0248afc43773
김아영 <thay123028@naver.com>
2025-01-11 03:24:12 +0900

    feat: 클릭 시 유저타입 전달

--- src/pages/SignUpModal.jsx
+++ src/pages/SignUpModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from 'styled-components';
-import { useUserInfo } from '../contexts/useUserInfo';
+import { useNavigate } from 'react-router-dom';
 import Modal from "../components/common/Modal";
 
 import IcWorker from '../assets/icons/ic_worker.png';
@@ -11,16 +11,16 @@ import { RiKakaoTalkFill } from "react-icons/ri";
 
 
 function SignUpModal({ onClose }) {
-  const [selectedType, setSelectedType] = useState(null);
+  const navigate = useNavigate();
 
-  const handleSelectUserType = (type) => {
-    setSelectedType(type);
+  const handleSignUp = (type) => {
+    navigate('/signup', { state: { userType: type } });
   };
 
   return (
       <Modal onClose={onClose} title="회원가입" width="600px">
         <UserType>
-          <TypeOption onClick={() => handleSelectUserType('worker')} selected={selectedType === 'worker'}>
+          <TypeOption onClick={() => handleSignUp('worker')}>
             <div class="option-info">
               <img id="worker" src={IcWorker} alt="worker" />
               <span>개인 회원</span>
@@ -44,7 +44,7 @@ function SignUpModal({ onClose }) {
               </SNSSignUpButton>
             </div>
           </TypeOption>
-          <TypeOption onClick={() => handleSelectUserType('owner')} selected={selectedType === 'owner'}>
+          <TypeOption onClick={() => handleSignUp('owner')}>
             <div class="option-info">
               <img id="owner" src={IcOwner} alt="owner" />
               <span>기업 회원</span>
@@ -73,6 +73,7 @@ const UserType = styled.div`
   width: 590px;
   margin-top: 15px;
   margin-bottom: 20px;
+  padding: 0 20px;
 `;
 
 const TypeOption = styled.div`
