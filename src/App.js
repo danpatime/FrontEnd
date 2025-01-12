f0d39789e3a5126feccf2e089079697e51623599
김아영 <thay123028@naver.com>
2025-01-13 00:10:41 +0900

    modify: MyStorePage 컴포넌트 참조 삭제

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import "./App.css";
 import Layout from "./components/layout/Layout";
 import Router from "./Router";
 
-import MypageSubMenu from "./components/common/MypageSubMenu";
-import MyStorePage from "./pages/MyPage/MyStorePage";
-
 function App() {
   return (
     <Layout>
       <Router />
-      <MyStorePage/>
     </Layout>
   );
 }
