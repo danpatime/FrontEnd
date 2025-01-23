1473f3a574430dfab3ce1e25b0c4ed62ae066fe3
김아영 <thay123028@naver.com>
2025-01-23 23:57:45 +0900

    feat: 나의 매장 라우터 추가

--- src/Router.js
+++ src/Router.js
@@ -8,6 +8,7 @@ import ChattingPage from './pages/ChattingPage';
 import MyResume from './pages/MyResume/MyResume';
 import SignUpPage from './pages/SignUpPage';
 import ContactDetailPage from './pages/MyPage/ContactDetailPage';
+import MyStorePage from './pages/MyPage/MyStore/MyStorePage';
 
 const AppRouter = () => {
   return (
@@ -21,6 +22,7 @@ const AppRouter = () => {
       <Route path="/mypage/resume" element={<MyResume />} />
       <Route path="/mypage/contracts" element={<MyStatusPage />} />
       <Route path="/contracts/:id" element={<ContactDetailPage />} />
+      <Route path="/mypage/mystore" element={<MyStorePage />} />
     </Routes>
   );
 };
