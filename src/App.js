75063cb195615fed4741cfeaea6c91c3987a46f9
aong13 <oaoa13@pukyong.ac.kr>
2025-02-16 14:48:17 +0900

    :bug: fix: 오타 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-mport './App.css';
+import './App.css';
 import Layout from './components/layout/Layout';
 import Router from './Router';
 import React from 'react';
 import { UserInfoProvider } from './contexts/UserInfoContext';
 import { ReviewProvider } from './contexts/ReviewInfoContext';
-
 
 function App() {
   return (
